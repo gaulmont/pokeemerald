5ca968369cc5dbeca0a5d2538776fde3f5aae33e
Sewef <elagroum@gmail.com>
2019-04-15 22:24:51 +0200

    struct BoxPokemon : unused to variant

--- include/pokemon.h
+++ include/pokemon.h
@@ -223,7 +223,7 @@ struct BoxPokemon
     u8 isBadEgg:1;
     u8 hasSpecies:1;
     u8 isEgg:1;
-    u8 unused:5;
+    u8 variant:5;
     u8 otName[PLAYER_NAME_LENGTH];
     u8 markings;
     u16 checksum;
