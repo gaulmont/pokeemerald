bff80e95c23e003f1f561a873282f89a1bf1650d
Sewef <elagroum@gmail.com>
2019-07-21 10:38:08 +0200

    Fix Pokemon naming screen

--- src/naming_screen.c
+++ src/naming_screen.c
@@ -1484,8 +1484,13 @@ static void sub_80E48E8(void)
 {
     u8 buffer[0x20];
 
-    StringCopy(buffer, gSpeciesNames[gNamingScreenData->monSpecies]);
-    StringAppendN(buffer, gNamingScreenData->template->title, 15);
+    #ifdef FRENCH
+        StringCopy(gStringVar1, gSpeciesNames[gNamingScreenData->monSpecies]);
+        StringExpandPlaceholders(buffer, gNamingScreenData->template->title);
+    #else
+        StringCopy(buffer, gSpeciesNames[gNamingScreenData->monSpecies]);
+        StringAppendN(buffer, gNamingScreenData->template->title, 15);
+    #endif
     FillWindowPixelBuffer(gNamingScreenData->windows[3], PIXEL_FILL(1));
     AddTextPrinterParameterized(gNamingScreenData->windows[3], 1, buffer, 8, 1, 0, 0);
     PutWindowTilemap(gNamingScreenData->windows[3]);
